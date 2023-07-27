ab5adee6e0397e4a001b3c7f083435210ddc0b21
Yosef Sokolov <77780368+JosefSo@users.noreply.github.com>
2023-07-27 14:55:41 +0300

    Delete Lightkurve.py

--- Lightkurve.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from lightkurve import search_targetpixelfile
-# import matplotlib.pyplot as plt
-
-pixelfile = search_targetpixelfile("KIC 8462852", quarter=16).download();
-pixelfile.plot(frame=1)
-
-plt.show()
